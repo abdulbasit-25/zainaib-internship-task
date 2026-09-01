7015c70b9de53786864a2f0c875aa128952de8a1
abdulbasit-025 <abdulbasit.alpha25@gmail.com>
2026-09-01 16:03:02 +0500

    Add enterprise dashboard index.js

---
+++ task-4-enterprise-dashboard/src/pages/index.js
@@ -0,0 +1,7 @@
+export { LoginPage } from "./LoginPage";
+export { DashboardPage } from "./DashboardPage";
+export { AnalyticsPage } from "./AnalyticsPage";
+export { UsersPage } from "./UsersPage";
+export { ProductsPage } from "./ProductsPage";
+export { TransactionsPage } from "./TransactionsPage";
+export { SettingsPage } from "./SettingsPage";
