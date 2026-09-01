c25da4da31148f3578d563a53f20fc806d42944e
abdulbasit-025 <abdulbasit.alpha25@gmail.com>
2026-09-01 16:01:35 +0500

    Add enterprise dashboard index.js

---
+++ task-4-enterprise-dashboard/src/components/index.js
@@ -0,0 +1,6 @@
+export { ProtectedRoute } from "./ProtectedRoute";
+export { Sidebar } from "./Sidebar";
+export { Navbar } from "./Navbar";
+export { MetricCard } from "./MetricCard";
+export { DataTable } from "./DataTable";
+export { ChartCard } from "./ChartCard";
